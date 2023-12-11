9fdbcbbf9bc13ca7e4c94123cae1e7bd3d17b2d5
Kirill <gatvik03@gmail.com>
2023-12-11 15:12:15 +0100

    Created recommendation repository

---
+++ Application/Contracts/Persistence/IRecommendationRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain;
+
+namespace Application.Contracts.Persistence;
+
+public interface IRecommendationRepository : IGenericRepository<Recommendation>
+{
+    Task<Recommendation?> GetRecommendationByKey(string key);
+}
---
+++ Persistence/Repositories/RecommendationRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Contracts.Persistence;
+using Domain;
+using Persistence.DatabaseContext;
+
+namespace Persistence.Repositories;
+
+public class RecommendationRepository : GenericRepository<Recommendation>, IRecommendationRepository
+{
+    public RecommendationRepository(DataContext context) : base(context)
+    {
+    }
+
+    public async Task<Recommendation?> GetRecommendationByKey(string key)
+    {
+        return await GetByPredicateAsync(r => r.Key == key);
+    }
+}
