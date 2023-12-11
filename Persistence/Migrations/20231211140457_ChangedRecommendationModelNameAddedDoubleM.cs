1f500ff201a914dde6e190454d17fdeae5ddaa11
Kirill <gatvik03@gmail.com>
2023-12-11 15:06:38 +0100

    To previous commit

---
+++ Persistence/Migrations/20231211140457_ChangedRecommendationModelNameAddedDoubleM.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedRecommendationModelNameAddedDoubleM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Recomendations",
+                table: "Recomendations");
+
+            migrationBuilder.RenameTable(
+                name: "Recomendations",
+                newName: "Recommendations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Recommendations",
+                table: "Recommendations",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Recommendations",
+                table: "Recommendations");
+
+            migrationBuilder.RenameTable(
+                name: "Recommendations",
+                newName: "Recomendations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Recomendations",
+                table: "Recomendations",
+                column: "Id");
+        }
+    }
+}
