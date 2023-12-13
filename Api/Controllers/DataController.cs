a8e754d00e856779510173a039bce3891afe6f85
Kirill <gatvik03@gmail.com>
2023-12-13 22:15:56 +0100

    Added export of DB to excel

---
+++ Api/Controllers/DataController.cs
@@ -0,0 +1,52 @@
+﻿using Api.Models;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Persistence.DatabaseContext;
+
+namespace Api.Controllers;
+
+[Route("api/v1/data")]
+[ApiController]
+public class DataController
+{
+    private readonly DataContext _context;
+
+    public DataController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("exportData")]
+    [Authorize(Roles = "Administrator")]
+    [FileDownload(FileName = "Data.xlsx")]
+    public async Task<IActionResult> ExportData()
+    {
+        var gyms = _context.Gyms.ToList();
+        var gymEnrollmentRequests = _context.GymEnrollmentRequests.ToList();
+        var measurements = _context.Measurements.ToList();
+        var members = _context.Members.ToList();
+        
+
+        using (var workbook = new XLWorkbook())
+        {
+            AddWorksheet(workbook, "Gyms", gyms);
+            AddWorksheet(workbook, "GymEnrollmentRequests", gymEnrollmentRequests);
+            AddWorksheet(workbook, "Measurements", measurements);
+            AddWorksheet(workbook, "Members", members);
+
+            using (var stream = new MemoryStream())
+            {
+                workbook.SaveAs(stream);
+                var content = stream.ToArray();
+                return new FileContentResult(content, "application/vnd.ms-excel");
+            }
+        }
+    }
+
+    private void AddWorksheet<T>(IXLWorkbook workbook, string worksheetName, IList<T> data)
+    {
+        var worksheet = workbook.Worksheets.Add(worksheetName);
+        worksheet.Cell(1, 1).InsertTable(data);
+    }
+}
---
+++ Api/Models/FileDownloadAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Api.Models;
+
+public class FileDownloadAttribute : ActionFilterAttribute
+{
+    public string FileName { get; set; } = null!;
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        context.HttpContext.Response.Headers.Add("Content-Disposition", $"attachment; filename={FileName}");
+    }
+}
